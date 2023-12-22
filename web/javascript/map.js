317013f96ada29a089cbd9a1573ec0d379ada406
Juris Ozolins <juris.ozolins2002@inbox.lv>
2023-12-22 09:50:32 +0200

    Pastel colors

--- web/javascript/map.js
+++ web/javascript/map.js
@@ -45,10 +45,6 @@ function drawLine(data, markers) {
   return lineFeature;
 }
 
-function getRandomInRange(range) {
-  return Math.floor(Math.random() * range);
-}
-
 function hslToHex(h, s, l) {
   l /= 100;
   const a = (s * Math.min(l, 1 - l)) / 100;
@@ -63,7 +59,7 @@ function hslToHex(h, s, l) {
 }
 
 function getColor(routeIndex, routeCount) {
-  color = hslToHex((routeIndex / (routeCount - 1)) * 300, 100, 50);
+  color = hslToHex((routeIndex / (routeCount - 1)) * 245, 100, 69);
   return color;
 }
 
